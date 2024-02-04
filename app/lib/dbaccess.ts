38467f0d9e8bb180d3f6c03a8ac2f47045ef7b9c
sasaki2110 <sasaki2110@gmail.com>
2024-02-04 14:36:25 +0900

    とりあえず、表示は完了。

--- app/lib/dbaccess.ts
+++ app/lib/dbaccess.ts
@@ -1,9 +1,19 @@
+// DBアクセスは async/await を使いたいから、別ファイルに切り出して、サーバーサイドで実行
 'use server'
 
+// vercel postgres 用のインポート
 import { sql } from '@vercel/postgres'
 
+// クエリー結果の型宣言
+// チートキー型
 export type Key = {
   key: string,
+}
+
+// チート型
+export type Cheat = {
+  title: string,
+  cheat: string,
 }
 
 /**
@@ -18,3 +28,13 @@ export async function GetCheatKeys() {
   return keys;
 }
 
+export async function GetCheatValues(key:string) {
+  console.log("111 key = [" + key + "]")
+  const data = await sql`select title, cheat from cheats where key = ${key}`
+  console.log("222")
+  console.log("data = [" , data, "]")
+
+  const cheats:Cheat[] = JSON.parse(JSON.stringify(data.rows))
+
+  return cheats;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+// onXXXXを使うとクライアントにせざるを得ない
+// 
+// 結果、メインの中でawaitが使えない。
+// 　　⇒　メインの中からasyncを呼ぶときは、useState を使うはめになる。
+//        （待ち合わせできないから、値が入ったらリレンダリングする為。）
+// 　　⇒  それを呼ぶのに useEffectを使うはめになる。
+// 
 'use client'
 
 import { useState , useEffect, Dispatch, SetStateAction} from 'react'
-import { Key, GetCheatKeys } from './lib/dbaccess'
+import { Key, GetCheatKeys, Cheat, GetCheatValues } from './lib/dbaccess'
 
 import {
   Select,
@@ -13,6 +20,13 @@ import {
   SelectValue,
 } from "@/components/ui/select"
 
+import { Textarea } from "@/components/ui/textarea"
+
+/**
+ * 内部でのキー取得処理（awaitで呼び出すための踏み台）
+ * ここは別に待ち合わせ出来ないから、promiseでも良い。
+ * @param setKeys 
+ */
 async function getKeys(setKeys: Dispatch<SetStateAction<Key[] | undefined>>) {
   console.log("local getKyes in...")
 
@@ -21,17 +35,37 @@ async function getKeys(setKeys: Dispatch<SetStateAction<Key[] | undefined>>) {
   setKeys(keys)
 }
 
+async function getCheats(key:string, setCheats: Dispatch<SetStateAction<Cheat[] | undefined>>) {
+  console.log("local getCheats in...")
+
+  const cheats = await GetCheatValues(key)
+
+  setCheats(cheats)
+}
+
+/**
+ * 主コンポーネント
+ * @returns 
+ */
 export default function Home() {
   console.log("home start")
+
+  // チートキー用のステート
   const [keys, setKeys] = useState<Key[] | undefined>(undefined)
 
-  console.log(keys)
+  // チートキー用のステート
+  const [cheats, setCheats] = useState<Cheat[] | undefined>(undefined)
 
-  //const handleSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
-  const handleSelect = (e) => {
-    console.log(JSON.stringify(e))
+
+  // セレクトが選択された時のハンドラ
+  const HandleSelect = (e:string) => {
+    // ここでチートを呼び出し、画面表示
+    console.log(e)
+    console.log("getKeys Call")
+    getCheats(e, setCheats)
   }
 
+  // 初期にチートキーを呼び出すエフェクト
   useEffect(() => {
     if(keys === undefined) {
       console.log("getKeys Call")
@@ -40,11 +74,10 @@ export default function Home() {
   }, [keys])
 
   return (
-    <div className="flex min-h-screen flex-col items-center p-24">
-      <div>
-        <>ポストぐレスに手が届くか？</>
-        <>もう、届いとるな。</>
-        <Select onValueChange={handleSelect}>
+    <div className="flex min-h-screen flex-col items-center p-24 ">
+      <div className='w-3/4'>
+        <h2>マイチートシート</h2>
+        <Select onValueChange={HandleSelect}>
           <SelectTrigger className="w-[180px]">
             <SelectValue placeholder="チートキーを選択してや！" />
           </SelectTrigger>
@@ -59,6 +92,15 @@ export default function Home() {
               </SelectGroup>
           </SelectContent>
         </Select>
+
+        {cheats && cheats.map((cheat) => (
+          <div className="mx-2 md:mx-8 my-4 md:my-8 px-2 md:px-8 py-4 md:py-8 bg-gray-cube border rounded-xl" key={1}>
+            <p className="text-grey-cube text-sm" >{cheat.title}</p>
+            <Textarea className="text-grey-cube text-lg w-3/4"
+                      value={cheat.cheat}
+                      />
+          </div> 
+        ))}
 
       </div>
     </div>
